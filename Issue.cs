2595999fe1f074eb75b9891628934117c89d2d1c
prtsie <yonhsup@mail.ru>
2025-04-23 17:18:19 +0300

    init

---
+++ Issue.cs
@@ -0,0 +1,6 @@
+namespace JiraCli;
+
+public class Issue
+{
+    public string Key { get; set; } = string.Empty;
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using JiraCli;
+
+Console.Write("Input mail >>> ");
+var mail = Console.ReadLine();
+Console.Write("Input token >>> ");
+var token = Console.ReadLine();
+Console.Write("Input domain >>> ");
+var domain = Console.ReadLine();
+
+var client = new Client(new HttpClient(), mail!, token!, domain!);
+var issues = await client.GetIssues();
+Console.WriteLine(string.Join('\n', issues.Select(x => x.Key)));
